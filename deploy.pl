08ac0a9ba6d6d4989cfff3574b28fbe81ac948d3
Meeji <sebastian.brabiner@gmail.com>
2014-05-17 23:20:32 +0100

    Added Bootstrap sources

--- deploy.pl
+++ deploy.pl
@@ -5,7 +5,7 @@ use File::Copy qw(copy);
 my $PATH        = ".\\oddeyedcat";
 my $DEPLOY_FROM = "build";
 my $DEPLOY_TO   = "..";
-my @TARGET_IGNORE = (".", "..", ".git", "oddeyedcat", "OLD", ".gitignore", "CNAME", "deploy.pl", "README.md");
+my @TARGET_IGNORE = ("bootstrap", ".", "..", ".git", "oddeyedcat", "OLD", ".gitignore", "CNAME", "deploy.pl", "README.md");
 my @SOURCE_IGNORE = ();
 
 my @deploy_source = Get_File_List($DEPLOY_FROM);
@@ -16,7 +16,7 @@ for (@deploy_target) {
 }
 
 for (@deploy_source) {
-  copy $PATH . "\\" . $DEPLOY_FROM . "\\" . $_ , $PATH . "\\" . $DEPLOY_TO . "\\" . $_;
+  copy $PATH . "\\" . $DEPLOY_FROM . "\\" . $_ , $PATH . "\\" . $DEPLOY_TO . "\\" . $_ unless $_ ~~ @TARGET_IGNORE;
 }
 
 sub Get_File_List {
